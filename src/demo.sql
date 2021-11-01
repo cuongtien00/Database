fbaa23a20acfc7651d5f429e1b3decd60c9d5eb1
cuongtien00 <cuonghiep000@gmail.com>
2021-11-01 11:33:44 +0700

    luyen tap cac ham sql

--- src/demo.sql
+++ src/demo.sql
@@ -19,6 +19,18 @@ join mark m on s.StudentID = m.StudentID
 group by s.studentID, s.studentName
 having DTBMAX >= all (select avg(Mark) from mark group by s.StudentID);
 
+# cac thong tin mon hoc co credit lon nhat
+select *, MAX(Credit) as 'Mon hoc credit lon nhat' from subject;
+# cac thong tin mon hoc co diem thi lon nhat
+select s.SubID,s.SubName,s.Status,s.Credit, MAX(Mark)as 'Diem thi lon nhat' from subject s
+join mark m on s.SubID = m.SubID
+group by s.SubID
+having MAX(Mark)>= all (select Max(Mark) from mark);
+
+select s.StudentID, s.StudentName, s.Address,s.ClassID, s.Phone,m.Mark, AVG(mark) as DTB from student s
+join mark m on s.StudentID = m.StudentID
+group by s.StudentID
+order by DTB desc ;
 
 
 
