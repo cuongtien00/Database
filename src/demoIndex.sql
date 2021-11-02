e017274c7efd5f58513b2ee4523cf7b00faee72c
cuongtien00 <cuonghiep000@gmail.com>
2021-11-02 10:42:46 +0700

    add bai thuc hanh tao procedure

--- src/demoIndex.sql
+++ src/demoIndex.sql
@@ -4,3 +4,18 @@ alter table customers add index customerIndex (customerName);
 explain select * from customers where customerName = 'Land of Toys Inc.';
 alter table customers add index index_full_name(contactFirstName,contactLastName);
 explain select * from  customers where contactFirstName = 'Jean'or  contactFirstName = 'King';
+
+create procedure findALlCustomers()
+begin
+    select * from customers;
+end;
+
+call findALlCustomers();
+
+drop procedure findALlCustomers;
+create procedure findAllCustomers()
+begin
+    select * from customers where customerNumber = 175;
+end;
+
+call findALlCustomers();
